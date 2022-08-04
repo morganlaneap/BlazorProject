c63aa28d10f021023c80505d970f9cdf8a0aed6b
Morgan Lane <morgan.lane@outlook.com>
2022-08-04 07:29:41 +0100

    Improved dom extensions

--- BlazorProject.UnitTests/TestUtils/DomExtensions.cs
+++ BlazorProject.UnitTests/TestUtils/DomExtensions.cs
@@ -1,27 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
 using Bunit;
 
 namespace BlazorProject.UnitTests.TestUtils
 {
     public static class DomExtensions
     {
-        public static IElement FindByText(this IRenderedFragment renderedFragment, string textToFind)
+        public static List<IElement> FindAllByText(this IRenderedFragment renderedFragment, string text)
         {
-            INode SearchNodes (INodeList nodeList)
+            var matchedElements = new List<IElement>();
+            void SearchNodes (INodeList nodeList)
             {
                 foreach (var node in nodeList)
                 {
-                    if (node.HasChildNodes)
-                    {
-                        var result = SearchNodes(node.ChildNodes);
-                        if (result != null) return result;
-                    }
-                    if (node.TextContent == textToFind) return node; 
+                    if (node.HasChildNodes) SearchNodes(node.ChildNodes);
+                    else if (node.TextContent == text && node.ParentElement != null && node.ParentElement is not IHtmlBodyElement) matchedElements.Add(node.ParentElement); 
                 }
-                return null;
             }
-            return SearchNodes(renderedFragment.Nodes)?.ParentElement;
+            SearchNodes(renderedFragment.Nodes);
+            if (matchedElements.Count == 0)
+            {
+                throw new Exception($"FindByText could not find the text '{text}' in the DOM.");
+            }
+            return matchedElements;
+        }
+        
+        public static IElement FindByText(this IRenderedFragment renderedFragment, string text)
+        {
+            var matchedElements = FindAllByText(renderedFragment, text);
+            return matchedElements.Count switch
+            {
+                > 1 => throw new Exception($"FindByText returned more than one element.\r\n\r\n{String.Join("\r\n", matchedElements.Select(x => x.OuterHtml))}"),
+                0 => throw new Exception($"FindByText could not find the text '{text}' in the DOM."),
+                _ => matchedElements[0]
+            };
         }
     }
 }
