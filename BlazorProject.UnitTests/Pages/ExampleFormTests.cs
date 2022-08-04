95c1380ab149899f7765ca393bba552ef1e3faef
Morgan Lane <morgan.lane@outlook.com>
2022-08-04 08:22:05 +0100

    Error state form tests

--- BlazorProject.UnitTests/Pages/ExampleFormTests.cs
+++ BlazorProject.UnitTests/Pages/ExampleFormTests.cs
@@ -28,5 +28,40 @@ namespace BlazorProject.UnitTests.Pages
 
             component.FindByText("Your form is Submitted").Should().NotBeNull();
         }
+        
+        [Fact]
+        public void InvalidEmailAddress()
+        {
+            using var context = new TestContext();
+            var component = context.RenderComponent<ExampleForm>();
+
+            var forenameInput = component.FindByLabelText("Forename:");
+            forenameInput.Change("John");
+
+            var surnameInput = component.FindByLabelText("Surname:");
+            surnameInput.Change("Smith");
+
+            var emailInput = component.FindByLabelText("Email Address:");
+            emailInput.Change("invalid email address");
+
+            var submitButton = component.FindByText("Submit");
+            submitButton.Click();
+
+            component.FindByText("The EmailAddress field is not a valid e-mail address.").Should().NotBeNull();
+        }
+        
+        [Fact]
+        public void RequiredFields()
+        {
+            using var context = new TestContext();
+            var component = context.RenderComponent<ExampleForm>();
+            
+            var submitButton = component.FindByText("Submit");
+            submitButton.Click();
+
+            component.FindByText("The Forename field is required.").Should().NotBeNull();
+            component.FindByText("The Surname field is required.").Should().NotBeNull();
+            component.FindByText("The EmailAddress field is required.").Should().NotBeNull();
+        }
     }
 }
